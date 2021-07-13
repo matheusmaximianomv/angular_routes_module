54fe3145e5a6725ae52a058f1529e3a5d516b703
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2021-07-13 20:31:42 -0300

    #89: Formulários reativos: Configuração (Módulo e Componente)

--- src/app/modules/pages/data-driven/data-driven.module.ts
+++ src/app/modules/pages/data-driven/data-driven.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DataDrivenComponent } from './data-driven.component';
 import { DataDrivenRoutingModule } from './data-driven-routing.module';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DataDrivenComponent],
   imports: [
     CommonModule,
     DataDrivenRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    HttpClientModule,
   ]
 })
 export class DataDrivenModule { }
