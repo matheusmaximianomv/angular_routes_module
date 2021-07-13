2d572c85ad2616e96d97e9c196d7bb5551d4202d
matheusmaximianomv <matheusmaximianomv@aluno.fapce.edu.br>
2021-07-13 09:02:32 -0300

    #79: Forms (template driven) Mostrando mensagens de erro de validação

--- src/app/modules/pages/template-driven/template-driven.component.ts
+++ src/app/modules/pages/template-driven/template-driven.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-interface UserForm {
-  name: string;
-  email: string;
-}
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
   styleUrls: ['./template-driven.component.less']
 })
 export class TemplateDrivenComponent implements OnInit {
-
-  public initialValues: UserForm = {
-    name: 'Matheus Maximiano de Melo Vieira',
-    email: 'matheus@email.com'
-  };
 
   constructor() { }
 
